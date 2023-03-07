7786c948d7ea01bb07f407dc8ec88823b8dc8027
Deborah Yscava <deborahly@gmail.com>
2023-03-07 15:24:02 -0500

    set up endpoints for agent

--- mern/server/routes/agent.route.js
+++ mern/server/routes/agent.route.js
@@ -1,73 +1,12 @@
 const express = require('express');
 const AgentController = require('../controllers/agent.controller');
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
-const recordRoutes = express.Router();
+const agentRoutes = express.Router();
 
-// This will help us connect to the database
-const dbo = require('../db/conn');
+agentRoutes.route('/agents').get(AgentController.getAgents);
+agentRoutes.route('/agent/:id').get(AgentController.getAgent);
+agentRoutes.route('/agent-create').post(AgentController.createAgent);
+agentRoutes.route('/agent-update/:id').post(AgentController.updateAgent);
+agentRoutes.route('/agent-delete').delete(AgentController.deleteAgent);
 
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require('mongodb').ObjectId;
-
-// This section will help you get a list of all the records.
-recordRoutes.route('/agents').get(AgentController.getAgents);
-
-// This section will help you get a single record by id
-recordRoutes.route('/record/:id').get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection('records').findOne(myquery, function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  });
-});
-
-// This section will help you create a new record.
-recordRoutes.route('/record/add').post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    name: req.body.name,
-    position: req.body.position,
-    level: req.body.level,
-  };
-  db_connect.collection('records').insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
-});
-
-// This section will help you update a record by id.
-recordRoutes.route('/update/:id').post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    $set: {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
-    },
-  };
-  db_connect
-    .collection('records')
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log('1 document updated');
-      response.json(res);
-    });
-});
-
-// This section will help you delete a record
-recordRoutes.route('/:id').delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection('records').deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log('1 document deleted');
-    response.json(obj);
-  });
-});
-
-module.exports = recordRoutes;
+module.exports = agentRoutes;
--- mern/server/services/agent.service.js
+++ mern/server/services/agent.service.js
@@ -9,4 +9,40 @@ async function getAgents() {
   }
 }
 
-module.exports = { getAgents };
+async function getAgent(id) {
+  try {
+    const dbResult = await AgentRepository.agentFindOne(id);
+    return dbResult;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function createAgent(agent) {
+  try {
+    const dbResult = await AgentRepository.agentInsertOne(agent);
+    return dbResult;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateAgent(id, agent) {
+  try {
+    const dbResult = await AgentRepository.agentUpdate(id, agent);
+    return dbResult;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function deleteAgent(id) {
+  try {
+    const dbResult = await AgentRepository.agentDelete(id);
+    return dbResult;
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = { getAgents, getAgent, createAgent, updateAgent, deleteAgent };
