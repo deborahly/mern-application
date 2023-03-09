ef50f2efb271efa85503c1da94bec0db6b356b1a
Deborah Yscava <deborahly@gmail.com>
2023-03-09 12:27:36 -0500

    modified routes config added AuthProvider and PrivateRoute

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
+import { AuthProvider } from 'react-auth-kit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <AuthProvider
+      authType={'cookie'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider> */}
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
