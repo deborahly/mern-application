aa822c0490783c5c732140702ef7eb584f05ce19
Deborah Yscava <deborahly@gmail.com>
2023-03-09 17:47:40 -0500

    removed comments

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -2,20 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { AuthProvider } from 'react-auth-kit';
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AuthProvider
-      authType={'cookie'}
-      authName={'_auth'}
-      cookieDomain={window.location.hostname}
-      cookieSecure={false}
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
