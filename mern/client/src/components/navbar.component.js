aadc26904661661fa446fda9590e641bba676ccd
Deborah Yscava <deborahly@gmail.com>
2023-03-09 22:53:37 -0500

    removed hamburger menu btn

--- mern/client/src/components/navbar.component.js
+++ mern/client/src/components/navbar.component.js
@@ -9,26 +9,14 @@ export default function Navbar() {
   return (
     <div>
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <NavLink className='navbar-brand' to='/'>
-          <img
-            alt='rocket-logo'
-            style={{ width: 12 + '%' }}
-            src={require('../images/rocketLogo.png')}
-          ></img>
-        </NavLink>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <div className='container-fluid'>
+          <NavLink className='navbar-brand' to='/'>
+            <img
+              alt='rocket-logo'
+              style={{ width: 12 + 'rem' }}
+              src={require('../images/rocketLogo.png')}
+            ></img>
+          </NavLink>
           <ul className='navbar-nav ml-auto'>
             <li className='nav-item'>
               <NavLink className='nav-link' to='/create'>
