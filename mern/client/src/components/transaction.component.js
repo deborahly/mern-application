44cefd9f41790e35db436939068d58d5459f51dc
Deborah Yscava <deborahly@gmail.com>
2023-03-14 01:07:05 -0400

    created transaction component

---
+++ mern/client/src/components/transaction.component.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+export default function Transaction(props) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const dateFormatter = date => {
+    date = new Date(date);
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', options);
+  };
+
+  return (
+    <tr>
+      <td>{dateFormatter(props.transaction.date)}</td>
+      <td>{formatter.format(props.transaction.amount)}</td>
+      <td>
+        {props.agent.firstName} {props.agent.lastName}
+      </td>
+    </tr>
+  );
+}
