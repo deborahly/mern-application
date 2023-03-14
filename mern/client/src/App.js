598e703908384c31ff277ed802dfff9edbd4cc60
Deborah Yscava <deborahly@gmail.com>
2023-03-14 01:14:07 -0400

    moved bootstrap import to App.js added routes for transaction & createTransaction

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthProvider, RequireAuth, useIsAuthenticated } from 'react-auth-kit';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Navbar from './components/navbar.component';
 import AgentList from './components/agentList/agentList.component';
+import TransactionList from './components/transactionList/transactionList.component';
 import Edit from './components/edit.component';
 import Create from './components/create.component';
+import CreateTransaction from './components/createTransaction.component';
 import Login from './components/login.component';
 import Logout from './components/logout.component';
 import Error from './components/error.component';
-import Home from './components/home.component';
+import Home from './components/home/home.component';
 
 const App = () => {
   const PrivateRoute = ({ Component }) => {
@@ -54,6 +57,22 @@ const App = () => {
                 element={
                   <RequireAuth loginPath='/login'>
                     <Create />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path='/transaction'
+                element={
+                  <RequireAuth loginPath='/login'>
+                    <TransactionList />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path='/create-transaction'
+                element={
+                  <RequireAuth loginPath='/login'>
+                    <CreateTransaction />
                   </RequireAuth>
                 }
               />
