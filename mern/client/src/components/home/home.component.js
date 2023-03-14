0bf062357b43b833e99f590883b3bea8f6304e0a
Deborah Yscava <deborahly@gmail.com>
2023-03-14 01:00:00 -0400

    created folder components/home added home.component & home.styles to new folder

---
+++ mern/client/src/components/home/home.component.js
@@ -0,0 +1,25 @@
+import Card from '../card/card.component';
+import './home.styles.css';
+
+function Home() {
+  return (
+    <div className='row home__row'>
+      <Card
+        className='col'
+        title='Agent Management'
+        image={require('../../images/agentCard.jpeg')}
+        text='View, create and delete agents.'
+        link='/agent'
+      ></Card>
+      <Card
+        className='col'
+        title='Transactions'
+        image={require('../../images/agentCard.jpeg')}
+        text='View and process transactions.'
+        link='/transaction'
+      ></Card>
+    </div>
+  );
+}
+
+export default Home;
