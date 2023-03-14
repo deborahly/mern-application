707f8f5f796d7b3840ed4a72b3fabdbd848ee5a3
Deborah Yscava <deborahly@gmail.com>
2023-03-14 00:55:04 -0400

    added sort in getData func func returning 10 most recent results

--- mern/server/services/transaction.service.js
+++ mern/server/services/transaction.service.js
@@ -4,7 +4,9 @@ const AgentService = require('../services/agent.service');
 async function getData() {
   try {
     const dbResult = await TransactionRepository.transactionFind();
-    const last10 = dbResult.filter((value, index) => index < 10);
+    const last10 = dbResult
+      .sort((a, b) => (a.date < b.date ? 1 : a.date > b.date ? -1 : 0))
+      .filter((value, index) => index < 10);
     const agents = await AgentService.getAgents();
     const data = { transactions: last10, agents };
     return data;
