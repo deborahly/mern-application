72a652a811148d8379c4c573b94ab8c09450770c
Deborah Yscava <deborahly@gmail.com>
2023-03-16 16:28:28 -0400

    moved module.exports up

--- mern/server/services/transaction.service.js
+++ mern/server/services/transaction.service.js
@@ -1,5 +1,6 @@
 const TransactionRepository = require('../repositories/transaction.repository');
-const AgentService = require('../services/agent.service');
+module.exports = { getData, createTransaction, deleteTransactions };
+const getAgents = require('../services/agent.service').getAgents;
 
 async function getData() {
   try {
@@ -7,7 +8,7 @@ async function getData() {
     const last10 = dbResult
       .sort((a, b) => (a.date < b.date ? 1 : a.date > b.date ? -1 : 0))
       .filter((value, index) => index < 10);
-    const agents = await AgentService.getAgents();
+    const agents = await getAgents();
     const data = { transactions: last10, agents };
     return data;
   } catch (err) {
@@ -26,4 +27,11 @@ async function createTransaction(transaction) {
   }
 }
 
-module.exports = { getData, createTransaction };
+async function deleteTransactions(agentId) {
+  try {
+    const dbResult = await TransactionRepository.transactionDeleteMany(agentId);
+    return dbResult;
+  } catch (err) {
+    throw err;
+  }
+}
