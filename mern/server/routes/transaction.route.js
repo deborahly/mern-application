4cbba4dd70d11d2487586dd5161ca9a596312a81
Deborah Yscava <deborahly@gmail.com>
2023-03-17 11:40:30 -0400

    added authorization mw to transaction routes

--- mern/server/routes/transaction.route.js
+++ mern/server/routes/transaction.route.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const TransactionController = require('../controllers/transaction.controller');
+const { authorization } = require('../middlewares/authorization.middleware');
 
 const transactionRoutes = express.Router();
 
-transactionRoutes.route('/transaction-data').get(TransactionController.getData);
+transactionRoutes
+  .route('/transaction-data')
+  .get(authorization, TransactionController.getData);
 transactionRoutes
   .route('/transaction')
-  .post(TransactionController.createTransaction);
+  .post(authorization, TransactionController.createTransaction);
 
 module.exports = transactionRoutes;
